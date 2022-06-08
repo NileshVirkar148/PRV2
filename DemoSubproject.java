08837f15e2ace37be587702c910f7d5fa0a24e86
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2022-06-08 10:51:29 +0530

    Update DemoSubproject.java

--- DemoSubproject.java
+++ DemoSubproject.java
@@ -13,7 +13,7 @@ public class DemoSubproject {
 	//multiple simultaneous callers may see partially initialized objects
 
  public static DemoSubproject getFoo() {     
-
+	int k_subproject;  int k_subproject2;
      if (foo==null) {
          foo = new DemoSubproject(); 
      }
